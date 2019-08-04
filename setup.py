5fa6d0c98a39054bca92934c2c432668e0c3723e
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-08-04 12:15:42 +0800

    Initialize repo

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 
 setup(
-    name='keras-xxx',
+    name='keras-gradient-accumulation',
     version='0.0.0',
     packages=find_packages(),
-    url='https://github.com/CyberZHG/keras-xxx',
+    url='https://github.com/CyberZHG/keras-gradient-accumulation',
     license='MIT',
     author='CyberZHG',
-    author_email='CyberZHG@gmail.com',
-    description='XXX implemented in Keras',
+    author_email='CyberZHG@users.noreply.github.com',
+    description='Gradient accumulation in Keras',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=install_requires,
